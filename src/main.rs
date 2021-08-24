20807866910b2cb2f3d97f95274f26ef97320db3
Kevin Berry <kpberry11@gmail.com>
2021-08-24 00:46:48 -0400

    Adds reverb demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use crate::raytracing::{make_box, profile_room, Visible, L, P, S, T};
+use crate::reverb::kernel_reverb;
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+
+pub mod raytracing;
+pub mod reverb;
+pub mod tuning;
+
+use reverb::demo;
+
 fn main() {
-    println!("Hello, world!");
+    demo()
 }
--- src/reverb.rs
+++ src/reverb.rs
@@ -43,3 +43,50 @@ pub fn _kernel_reverb(samples: &Vec<f64>, kernel: &Vec<f64>) -> Vec<f64> {
     }
     result
 }
+
+
+pub fn demo() {
+    let room = make_box(
+        100.,
+        100.,
+        100.,
+        P {
+            x: 0.,
+            y: 0.,
+            z: 0.,
+        },
+        true,
+    );
+    let room_ref = room.iter().map(|r| r as &dyn Visible).collect();
+    let speaker = P {
+        x: 50.,
+        y: 95.,
+        z: 5.,
+    };
+    let microphone = S {
+        c: P {
+            x: 50.,
+            y: 95.,
+            z: 95.,
+        },
+        r: 5.,
+    };
+    let kernel = profile_room(
+        &room_ref,
+        &speaker,
+        &microphone,
+        100000,
+        1000,
+        10.,
+        343.,
+        0.05,
+        1000.,
+        44100.,
+    );
+    let out_str = format!("{:?}", kernel);
+    let path = Path::new("kernel.json");
+    let mut file = File::create(path).unwrap();
+    file.write_all(out_str.as_bytes());
+    // let audio = audio::Audio::read("/home/kpberry/Music/the world is not as large as i thought it was/3.wav");
+    // let reverbed = reverb::kernel_reverb(&audio.samples, &kernel);
+}
