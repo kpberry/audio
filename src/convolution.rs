acad01007e54fa9553c14556c46e5c72e342d15d
Kevin Berry <kpberry11@gmail.com>
2021-12-28 20:41:26 -0500

    Make planner an argument to convolve

--- src/convolution.rs
+++ src/convolution.rs
@@ -4,11 +4,11 @@ use num::integer::Roots;
 use num::Zero;
 use rustfft::{Direction, Fft, FftPlanner, num_complex::Complex};
 
-pub fn fft_convolve(signal: &Vec<Complex<f64>>, kernel: &Vec<Complex<f64>>) -> Vec<Complex<f64>> {
+pub fn fft_convolve(signal: &Vec<Complex<f64>>, kernel: &Vec<Complex<f64>>,
+                    planner: &mut FftPlanner<f64>) -> Vec<Complex<f64>> {
     // the lengths here are very important; don't change them unless you know what you're doing
     let len = signal.len() + kernel.len() - 1;
     let buf_len = len.next_power_of_two();
-    let mut planner = FftPlanner::new();
     let fft = planner.plan_fft_forward(buf_len);
     let ifft = planner.plan_fft_inverse(buf_len);
 
@@ -33,10 +33,12 @@ pub fn fft_convolve(signal: &Vec<Complex<f64>>, kernel: &Vec<Complex<f64>>) -> V
     normed_product.into_iter().take(len).collect()
 }
 
-pub fn rfft_convolve(signal: &Vec<f64>, kernel: &Vec<f64>) -> Vec<f64> {
+pub fn rfft_convolve(signal: &Vec<f64>, kernel: &Vec<f64>,
+                     planner: &mut FftPlanner<f64>) -> Vec<f64> {
     fft_convolve(
         &signal.iter().map(|&x| Complex::new(x, 0.0)).collect(),
         &kernel.iter().map(|&y| Complex::new(y, 0.0)).collect(),
+        planner,
     ).iter().map(|p| p.re).collect()
 }
 
@@ -53,6 +55,7 @@ fn test_rfft_convolve() {
         &vec![
             1., 2., 3., 4., 5., 6., 7., 8., 1., 2., 3., 4., 5., 6., 7., 8.,
         ],
+        &mut FftPlanner::new(),
     );
     let expected = vec![
         1.00, 4.00, 10.00, 20.00, 35.00, 56.00, 84.00, 120.00, 149.00, 172.00, 190.00,
