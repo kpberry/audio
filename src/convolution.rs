a0ed5f987460287c2be94f1c7629dd3108ce6be4
Kevin Berry <kpberry11@gmail.com>
2021-12-29 23:43:44 -0500

    Make rfft_convolve_real_time more similar to vst code

--- src/convolution.rs
+++ src/convolution.rs
@@ -2,6 +2,7 @@ use std::f64::consts::PI;
 
 use num::integer::Roots;
 use num::Zero;
+use rand::seq::index::sample;
 use rustfft::{Direction, Fft, FftPlanner, num_complex::Complex};
 
 pub fn fft_convolve(signal: &Vec<Complex<f64>>, kernel: &Vec<Complex<f64>>,
@@ -73,40 +74,39 @@ fn test_rfft_convolve() {
     assert!(convolved.iter().zip(expected).all(|(a, b)| (a - b).abs() < 1e-7));
 }
 
-pub fn fft_convolve_real_time(signal: &Vec<Complex<f64>>, kernel: &Vec<Complex<f64>>,
-                              sample_size: usize, planner: &mut FftPlanner<f64>) -> Vec<Complex<f64>> {
-    let start_index = signal.len().saturating_sub(kernel.len() + sample_size);
-    let kernel_samples: Vec<Complex<f64>> = signal.iter().skip(start_index).cloned().collect();
-    let response = fft_convolve(&kernel_samples, &kernel, planner);
-    response.iter().skip(response.len() - kernel.len() - sample_size + 1).take(sample_size).cloned().collect()
-}
+fn rfft_convolve_real_time(signal: &Vec<f64>, kernel: &Vec<f64>, sample_size: usize,
+                           planner: &mut FftPlanner<f64>) -> Vec<f64> {
+    let mut input_buffers = Vec::new();
+    let mut output_buffers = Vec::new();
+    for i in (0..signal.len()).step_by(sample_size) {
+        let input_buffer: Vec<f64> = signal.iter().skip(i).take(sample_size).cloned().collect();
+        let buf_len = input_buffer.len();
+        input_buffers.push(input_buffer);
 
-pub fn rfft_convolve_real_time(signal: &Vec<f64>, kernel: &Vec<f64>, sample_size: usize,
-                               planner: &mut FftPlanner<f64>) -> Vec<f64> {
-    let start_index = signal.len().saturating_sub(kernel.len() + sample_size);
-    let kernel_samples: Vec<f64> = signal.iter().skip(start_index).cloned().collect();
-    let response = rfft_convolve(&kernel_samples, &kernel, planner);
-    response.iter().skip(response.len() - kernel.len() - sample_size + 1).take(sample_size).cloned().collect()
-}
+        let output_buffer: Vec<f64> = vec![0.0; buf_len];
+        output_buffers.push(output_buffer);
+    }
 
+    let mut signal_buffer = vec![0.0; kernel.len()];
+    for (input_buffer, output_buffer) in input_buffers.iter().zip(output_buffers.iter_mut()) {
+        let start_index = signal_buffer.len().saturating_sub(kernel.len());
+        signal_buffer = signal_buffer.iter()
+            .skip(start_index)
+            .cloned()
+            .chain(input_buffer.iter().map(|&f| f))
+            .collect();
+        let response = rfft_convolve(&signal_buffer, kernel, planner);
+        for (&r, o) in response.iter().skip(kernel.len()).zip(output_buffer.iter_mut()) {
+            *o = r;
+        }
+    }
+
+    output_buffers.iter().cloned().flatten().collect()
+}
 
 #[test]
 fn test_rfft_convolve_real_time() {
-    fn full_rfft_convolve_real_time(signal: &Vec<f64>, kernel: &Vec<f64>, sample_size: usize,
-                                    planner: &mut FftPlanner<f64>) -> Vec<f64> {
-        let mut signal_buffer = vec![0.0; kernel.len()];
-        let mut output_buffer = Vec::new();
-        for i in (0..signal.len()).step_by(sample_size) {
-            let samples: Vec<f64> = signal.iter().skip(i).take(sample_size).cloned().collect();
-            let samples_len = samples.len();
-            signal_buffer.extend(samples);
-            let response = rfft_convolve_real_time(&signal_buffer, kernel, samples_len, planner);
-            output_buffer.extend(response);
-        }
-        output_buffer
-    }
-
-    let convolved = full_rfft_convolve_real_time(
+    let convolved = rfft_convolve_real_time(
         &vec![
             1., 2., 3., 4., 5., 6., 7., 8., 1., 2., 3., 4., 5., 6., 7., 8.,
             1., 2., 3., 4., 5., 6., 7., 8., 1., 2., 3., 4., 5., 6., 7., 8.,
