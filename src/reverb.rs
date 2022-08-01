4671b23ca42c7c57d8b9019e14f3e11ab37d1dde
Kevin Berry <kpberry11@gmail.com>
2022-08-01 01:49:11 -0400

    Improve reverb demo

--- src/reverb.rs
+++ src/reverb.rs
@@ -1,16 +1,15 @@
-use std::{fs::File, io::Write, path::Path};
-use std::cmp::Ordering;
+use std::{fs::File, path::Path};
+use std::time::Instant;
+use rustfft::FftPlanner;
+
 use wav::BitDepth;
+
 use crate::convolution::rfft_convolve;
-
-use crate::raytracing::{make_box, profile_room, Visible, P, S};
-
-pub fn kernel_reverb(samples: &Vec<f64>, kernel: &Vec<f64>) -> Vec<f64> {
-    rfft_convolve(samples, kernel)
-}
+use crate::geometry::{ make_box, P, Q, S, Visible};
+use crate::raytracing::profile_room;
 
 
-pub fn demo() {
+pub fn demo(path: &Path) {
     let room = make_box(
         100.,
         100.,
@@ -19,23 +18,21 @@ pub fn demo() {
             x: 0.,
             y: 0.,
             z: 0.,
-        },
-        true,
+        }
     );
     let room_ref = room.iter().map(|r| r as &dyn Visible).collect();
     let speaker = P {
         x: 50.,
-        y: 95.,
+        y: 5.,
         z: 5.,
     };
-    let microphone = S {
-        c: P {
-            x: 50.,
-            y: 95.,
-            z: 95.,
-        },
-        r: 5.,
-    };
+    let microphone = Q::cw(
+        P { x: 48., y: 6., z: 95. },
+        P { x: 52., y: 6., z: 95. },
+        P { x: 52., y: 4., z: 95. },
+        P { x: 48., y: 4., z: 95. }
+    );
+    let t = Instant::now();
     let kernel = profile_room(
         &room_ref,
         &speaker,
@@ -48,10 +45,40 @@ pub fn demo() {
         1000.,
         44100.,
     );
-    let out_str = format!("{:?}", kernel);
-    let path = Path::new("kernel.json");
-    let mut file = File::create(path).unwrap();
-    file.write_all(out_str.as_bytes());
-    // let audio = audio::Audio::read("/home/kpberry/Music/the world is not as large as i thought it was/3.wav");
-    // let reverbed = reverb::kernel_reverb(&audio.samples, &kernel);
+    println!("{}", t.elapsed().as_secs_f32());
+
+    let mut in_file = File::open(path).unwrap();
+    let (header, data) = wav::read(&mut in_file).unwrap();
+    println!("{:?}", header);
+    let samples: Vec<f64> = match data {
+        BitDepth::Eight(ref _samples) => {_samples.iter().cloned().map(|s| s as f64).collect()},
+        BitDepth::Sixteen(ref _samples) => {_samples.iter().cloned().map(|s| s as f64).collect()},
+        BitDepth::TwentyFour(ref _samples) => {_samples.iter().cloned().map(|s| s as f64).collect()},
+        BitDepth::ThirtyTwoFloat(ref _samples) => {_samples.iter().cloned().map(|s| s as f64).collect()},
+        BitDepth::Empty => { panic!("Could not load samples"); }
+    };
+
+    fn normalize(xs: &[f64]) -> Vec<f64> {
+        let norm = 1. / xs.iter().fold(0., |a: f64, &b| a.max(b));
+        xs.iter().map(|x| x * norm).collect()
+    }
+
+    let samples = normalize(&samples);
+    let kernel = normalize(&kernel);
+    let mut planner: FftPlanner<f64> = FftPlanner::new();
+    // let reverbed1 = rfft_convolve_real_time(&samples, &kernel, 100000, &planner);
+    let reverbed = rfft_convolve(&samples, &kernel, &mut planner);
+    // assert!(reverbed1.iter().zip(reverbed.iter()).all(|(a, b)| (a - b).abs() < 1e-7));
+    let reverbed = normalize(&reverbed);
+
+    let out_data = match data {
+        BitDepth::Eight(_) => {BitDepth::Eight(reverbed.iter().cloned().map(|s| ((s + 0.5) * 128.) as u8).collect())},
+        BitDepth::Sixteen(_) => {BitDepth::Sixteen(reverbed.iter().cloned().map(|s| (s * 32767.) as i16).collect())},
+        BitDepth::TwentyFour(_) => {BitDepth::TwentyFour(reverbed.iter().cloned().map(|s| (s * 8388607.) as i32).collect())},
+        BitDepth::ThirtyTwoFloat(_) => {BitDepth::ThirtyTwoFloat(reverbed.iter().cloned().map(|s| s as f32).collect())},
+        BitDepth::Empty => { panic!("Could not load samples"); }
+    };
+
+    let mut out_file = File::create("she won't be there_reverb.wav").unwrap();
+    wav::write(header, &out_data, &mut out_file).unwrap();
 }
